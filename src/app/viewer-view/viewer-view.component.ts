0741533a447a5c56ab990798f1d874a2fec82196
DrJKL <DrJKL0424@gmail.com>
2023-12-30 13:31:22 -0800

    Add initial exif parsing

--- src/app/viewer-view/viewer-view.component.ts
+++ src/app/viewer-view/viewer-view.component.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
   effect,
   signal,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import 'core-js/actual/array/from-async';
+import ExifReader, { ExifTags } from 'exifreader';
 import { showDirectoryPicker } from 'file-system-access';
 import { ButtonModule } from 'primeng/button';
 import { ImageModule } from 'primeng/image';
@@ -41,7 +40,9 @@ export class ViewerViewComponent {
   readonly itemsPerPage = signal(9);
   readonly columns = signal(3);
 
-  readonly imageSlice = signal<Array<readonly [string, string]>>([]);
+  readonly imageSlice = signal<
+    Array<readonly [string, string, ExifReader.Tags]>
+  >([]);
 
   readonly updateSliceEffect = effect(() => {
     const files = this.files();
@@ -50,21 +51,30 @@ export class ViewerViewComponent {
       this.first() + this.itemsPerPage()
     );
     Promise.all(
-      filesSlice.map(async (handle) => {
-        const file = await handle.getFile();
-        return [handle.name, URL.createObjectURL(file)] as const;
-      })
+      filesSlice.map(
+        async (handle): Promise<readonly [string, string, ExifReader.Tags]> => {
+          const file = await handle.getFile();
+          return [
+            handle.name,
+            URL.createObjectURL(file),
+            await ExifReader.load(file),
+          ] as const;
+        }
+      )
     ).then((slice) => this.imageSlice.set(slice));
   });
 
   async openFolderSelector(_$event: MouseEvent) {
     const directoryHandle = await showDirectoryPicker();
+    if (!directoryHandle) {
+      return;
+    }
     this.loading.set(true);
     this.files.set([]);
     this.page.set(0);
     this.first.set(0);
     const buffer: FileSystemFileHandle[] = [];
-    for await (const file of await getFilesRecursively(directoryHandle)) {
+    for await (const file of getFilesRecursively(directoryHandle)) {
       if (buffer.length >= 40) {
         this.files.update((files) => [...files, ...buffer]);
         buffer.splice(0, buffer.length);
@@ -86,27 +96,39 @@ export class ViewerViewComponent {
       this.itemsPerPage.set($event.rows);
     }
   }
+
+  checkExifData($event: MouseEvent, tags: ExifReader.Tags) {
+    console.log({ tags });
+    const { prompt, workflow } = tags;
+    if (prompt) {
+      const promptJSON = JSON.parse(prompt.value);
+      console.log(promptJSON);
+    }
+    if (workflow) {
+      const workflowJSON = JSON.parse(workflow.value);
+      console.log(workflowJSON);
+    }
+  }
 }
 
-function getAllFilesRecursively(
-  directory: FileSystemDirectoryHandle
-): Promise<FileSystemFileHandle[]> {
-  // @ts-ignore
-  return Array.fromAsync(getFilesRecursively(directory));
-}
+const ALLOWED_EXTENSIONS = ['png', 'jpg', 'mp4', 'gif'] as const;
 
 async function* getFilesRecursively(
   entry: FileSystemFileHandle | FileSystemDirectoryHandle
 ): AsyncGenerator<FileSystemFileHandle> {
-  if (entry.kind === 'file') {
-    const file = await entry.getFile();
-    if (file !== null) {
-      // file.relativePath = getRelativePath(entry);
-      yield entry;
-    }
-  } else if (entry.kind === 'directory') {
-    for await (const handle of entry.values()) {
-      yield* getFilesRecursively(handle);
-    }
+  switch (entry.kind) {
+    case 'file':
+      const file = await entry.getFile();
+      const extension = entry.name.slice(entry.name.lastIndexOf('.') + 1);
+      const valid = ALLOWED_EXTENSIONS.some((ext) => ext === extension);
+      if (file !== null && valid) {
+        yield entry;
+      }
+      break;
+    case 'directory':
+      for await (const handle of entry.values()) {
+        yield* getFilesRecursively(handle);
+      }
+      break;
   }
 }
